9d02f13714162c60a4f4237d38c00ea0b024e9d6
marco <marco@MTA_LAPTOP>
2024-12-03 18:06:29 -0700

    cambio color botón axtualizar - marcos

--- Proyecto_TBD/Clientes_Forms.Designer.cs
+++ Proyecto_TBD/Clientes_Forms.Designer.cs
@@ -49,72 +49,77 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 312);
+            this.label1.Location = new System.Drawing.Point(50, 250);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.Size = new System.Drawing.Size(59, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre:";
             // 
             // clave_cliente_lbl
             // 
             this.clave_cliente_lbl.AutoSize = true;
-            this.clave_cliente_lbl.Location = new System.Drawing.Point(27, 264);
+            this.clave_cliente_lbl.Location = new System.Drawing.Point(24, 211);
             this.clave_cliente_lbl.Name = "clave_cliente_lbl";
-            this.clave_cliente_lbl.Size = new System.Drawing.Size(98, 20);
+            this.clave_cliente_lbl.Size = new System.Drawing.Size(84, 16);
             this.clave_cliente_lbl.TabIndex = 1;
             this.clave_cliente_lbl.Text = "Clave cliente";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(54, 362);
+            this.label3.Location = new System.Drawing.Point(48, 290);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.Size = new System.Drawing.Size(61, 16);
             this.label3.TabIndex = 2;
             this.label3.Text = "Telefono";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(46, 410);
+            this.label4.Location = new System.Drawing.Point(41, 328);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 20);
+            this.label4.Size = new System.Drawing.Size(67, 16);
             this.label4.TabIndex = 3;
             this.label4.Text = "Direccion:";
             // 
             // nombre_cliente_txt
             // 
-            this.nombre_cliente_txt.Location = new System.Drawing.Point(158, 312);
+            this.nombre_cliente_txt.Location = new System.Drawing.Point(140, 250);
+            this.nombre_cliente_txt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.nombre_cliente_txt.Name = "nombre_cliente_txt";
-            this.nombre_cliente_txt.Size = new System.Drawing.Size(195, 26);
+            this.nombre_cliente_txt.Size = new System.Drawing.Size(174, 22);
             this.nombre_cliente_txt.TabIndex = 4;
             // 
             // clave_cliente_txt
             // 
-            this.clave_cliente_txt.Location = new System.Drawing.Point(158, 264);
+            this.clave_cliente_txt.Location = new System.Drawing.Point(140, 211);
+            this.clave_cliente_txt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.clave_cliente_txt.Name = "clave_cliente_txt";
-            this.clave_cliente_txt.Size = new System.Drawing.Size(141, 26);
+            this.clave_cliente_txt.Size = new System.Drawing.Size(126, 22);
             this.clave_cliente_txt.TabIndex = 5;
             // 
             // telefono_txt
             // 
-            this.telefono_txt.Location = new System.Drawing.Point(158, 362);
+            this.telefono_txt.Location = new System.Drawing.Point(140, 290);
+            this.telefono_txt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.telefono_txt.Name = "telefono_txt";
-            this.telefono_txt.Size = new System.Drawing.Size(141, 26);
+            this.telefono_txt.Size = new System.Drawing.Size(126, 22);
             this.telefono_txt.TabIndex = 6;
             // 
             // direccion_txt
             // 
-            this.direccion_txt.Location = new System.Drawing.Point(158, 410);
+            this.direccion_txt.Location = new System.Drawing.Point(140, 328);
+            this.direccion_txt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.direccion_txt.Name = "direccion_txt";
-            this.direccion_txt.Size = new System.Drawing.Size(195, 26);
+            this.direccion_txt.Size = new System.Drawing.Size(174, 22);
             this.direccion_txt.TabIndex = 7;
             // 
             // agregar_btn
             // 
-            this.agregar_btn.Location = new System.Drawing.Point(31, 539);
+            this.agregar_btn.Location = new System.Drawing.Point(28, 431);
+            this.agregar_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.agregar_btn.Name = "agregar_btn";
-            this.agregar_btn.Size = new System.Drawing.Size(94, 33);
+            this.agregar_btn.Size = new System.Drawing.Size(84, 26);
             this.agregar_btn.TabIndex = 8;
             this.agregar_btn.Text = "Agregar";
             this.agregar_btn.UseVisualStyleBackColor = true;
@@ -122,27 +127,31 @@
             // mostrar_btn
             // 
             this.mostrar_btn.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.mostrar_btn.Location = new System.Drawing.Point(175, 539);
+            this.mostrar_btn.Location = new System.Drawing.Point(156, 431);
+            this.mostrar_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.mostrar_btn.Name = "mostrar_btn";
-            this.mostrar_btn.Size = new System.Drawing.Size(94, 33);
+            this.mostrar_btn.Size = new System.Drawing.Size(84, 26);
             this.mostrar_btn.TabIndex = 9;
             this.mostrar_btn.Text = "Mostrar";
             this.mostrar_btn.UseVisualStyleBackColor = false;
             // 
             // actualizar_btn
             // 
-            this.actualizar_btn.Location = new System.Drawing.Point(322, 539);
+            this.actualizar_btn.BackColor = System.Drawing.Color.RosyBrown;
+            this.actualizar_btn.Location = new System.Drawing.Point(286, 431);
+            this.actualizar_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.actualizar_btn.Name = "actualizar_btn";
-            this.actualizar_btn.Size = new System.Drawing.Size(94, 33);
+            this.actualizar_btn.Size = new System.Drawing.Size(84, 26);
             this.actualizar_btn.TabIndex = 10;
             this.actualizar_btn.Text = "Actualizar";
-            this.actualizar_btn.UseVisualStyleBackColor = true;
+            this.actualizar_btn.UseVisualStyleBackColor = false;
             // 
             // eliminar_btn
             // 
-            this.eliminar_btn.Location = new System.Drawing.Point(478, 539);
+            this.eliminar_btn.Location = new System.Drawing.Point(425, 431);
+            this.eliminar_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.eliminar_btn.Name = "eliminar_btn";
-            this.eliminar_btn.Size = new System.Drawing.Size(94, 33);
+            this.eliminar_btn.Size = new System.Drawing.Size(84, 26);
             this.eliminar_btn.TabIndex = 11;
             this.eliminar_btn.Text = "Eliminar";
             this.eliminar_btn.UseVisualStyleBackColor = true;
@@ -150,44 +159,44 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(10, 9);
+            this.label5.Location = new System.Drawing.Point(9, 7);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(176, 20);
+            this.label5.Size = new System.Drawing.Size(148, 16);
             this.label5.TabIndex = 12;
             this.label5.Text = "Pardini Felix Bruno Alan";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(10, 39);
+            this.label6.Location = new System.Drawing.Point(9, 31);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(228, 20);
+            this.label6.Size = new System.Drawing.Size(190, 16);
             this.label6.TabIndex = 13;
             this.label6.Text = "Orosco Bustamamante Andres";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(10, 72);
+            this.label7.Location = new System.Drawing.Point(9, 58);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(232, 20);
+            this.label7.Size = new System.Drawing.Size(195, 16);
             this.label7.TabIndex = 14;
             this.label7.Text = "Soto Belmonte Cesar Alejandro";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(10, 109);
+            this.label8.Location = new System.Drawing.Point(9, 87);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(176, 20);
+            this.label8.Size = new System.Drawing.Size(148, 16);
             this.label8.TabIndex = 15;
             this.label8.Text = "Pardini Felix Bruno Alan";
             // 
             // Clientes_Forms
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 729);
+            this.ClientSize = new System.Drawing.Size(711, 583);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -204,6 +213,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.clave_cliente_lbl);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Clientes_Forms";
             this.Text = "Clientes_Forms";
             this.Load += new System.EventHandler(this.Clientes_Forms_Load);
